23a467ce2cfa22273356a4c038d59bce8aa8bc2d
MohammadWolf95 <abdulavadudovmagomed@mail.ru>
2018-03-06 02:02:19 +0300

    next commit

--- lib.cpp
+++ lib.cpp
@@ -3,7 +3,15 @@
 #include "lib.h"
 
 
-int version()
+int patch_version()
 {
+	return PROJECT_VERSION_PATCH;
+}
+
+int minor_version(){
 	return PROJECT_VERSION_MINOR;
+}
+
+int major_version(){
+	return PROJECT_VERSION_MAJOR;
 }
